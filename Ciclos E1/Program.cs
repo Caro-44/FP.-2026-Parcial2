fe0ba9e776891b3a32f915af61de46a009e7d3eb
Caro <fllcortizv@gmail.com>
2026-09-16 02:11:36 -0600

    Ejercicio 1: Ciclos While

---
+++ Ciclos E1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* EJERCICIO 1
+Genera un programa que muestre un log de combate donde el jugador realiza 5 puntos de daño al enemigo cada turno.
+Probar con diferentes valores iniciales de vida (-50, 5, 0).
+*/
+
+int    daño        = 5;
+Random rnd         = new Random();
+int    numero      = rnd.Next(1,50);
+int    vidaEnemigo = numero;
+int    vidaFinal;
+
+while (vidaEnemigo > 0)
+{
+
+    Console.WriteLine("La vida del enemigo es: " + vidaEnemigo);
+    vidaEnemigo = vidaEnemigo - daño;
+    Console.WriteLine("La vida del enemigo despúes del ataque es: " + vidaEnemigo);
+    Console.WriteLine("\n");
+}
+
+Console.WriteLine("¡Haz derrotado al enemigo!");
+
+Console.WriteLine("\n\n");
