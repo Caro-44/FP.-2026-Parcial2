5290f263dad0839fb5f486c3d2412c00712a4db6
Caro <fllcortizv@gmail.com>
2026-09-16 02:12:28 -0600

    Ejercicio 3: Ciclos For

---
+++ Ciclos E3/Program.cs
@@ -0,0 +1,15 @@
+﻿/* EJERCICIO 3
+Generar un programa que muestre la tabla del 7, del 0 al 100 de forma ordenada.
+*/
+
+Console.WriteLine("La tabla del siete");
+Console.WriteLine("\n");
+
+var siete = 7;
+
+for (int tabla = 0;
+     tabla <= 100;
+     tabla++)
+{
+    Console.WriteLine(siete + " * " + tabla + " = " + tabla * 7);
+}
